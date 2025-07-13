241d861c078088072390273766f10755f4b33c6f
hstewart38 <hstewart38@gatech.edu>
2025-07-13 18:08:10 -0400

    Remove duplicate build script. Add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+Create new pages by adding new markdown files in ./markdown/ directory. Then run ./build/md-to-html.zsh to convert that markdown into html.
--- build/md-to-html.zsh
+++ build/md-to-html.zsh
@@ -5,11 +5,11 @@ PARENT_DIR="${SCRIPT_DIR:h}"
 
 for file in "$PARENT_DIR"/markdown/*.md; do
     filename=$(basename "$file" .md)
-    pandoc "$file" -s -t html5 --katex --css "$SCRIPT_DIR"/style.css --template "$SCRIPT_DIR"/template.html -o "$PARENT_DIR"/"$filename".html
+    pandoc "$file" \
+        -s \
+        -t html5 \
+        --katex \
+        --css "$SCRIPT_DIR"/style.css \
+        --template "$SCRIPT_DIR"/template.html \
+        -o "$PARENT_DIR"/"$filename".html
 done
-
-
-# for file in "$PARENT_DIR"/markdown/*.md; do
-#     filename=$(basename "$file" .md)
-#     pandoc "$file" -s -t html5 --katex --css https://lairdstewart.com/style.css --template "$SCRIPT_DIR"/template.html -o "$PARENT_DIR"/"$filename".html
-# done
